23d8f2a5f83c785718b91edf62299702e5132eaf
Victor Wong <victorhim6@gmail.com>
2026-01-17 03:03:37 +0800

    Rework the app.html to be a login page

--- i18n-app/src/app/app.ts
+++ i18n-app/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('i18n-app');
+  username = '';
+  password = '';
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // Add your login logic here
+  }
 }
