174df37bec9452ab60c63bedff8db2f3302ebec5
Victor Wong <victorhim6@gmail.com>
2026-01-17 09:40:22 +0800

    Apply ngx-translate into login page

--- i18n-app/src/app/app.config.ts
+++ i18n-app/src/app/app.config.ts
@@ -3,10 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideTranslateService(),
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: '.json'
+    }),
   ]
 };
--- i18n-app/src/app/app.ts
+++ i18n-app/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,9 +14,18 @@ export class App {
   username = '';
   password = '';
 
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
   login() {
     console.log('Username:', this.username);
     console.log('Password:', this.password);
     // Add your login logic here
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
   }
 }
